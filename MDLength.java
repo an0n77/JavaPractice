40265100e64bf473c2545deccb6fb0a21b380308
anishmaharjan <003anish@gmail.com>
2019-05-20 08:36:39 +0545

    Printing in Array form & M-D Array Dynamic Length

--- MDLength.java
+++ MDLength.java
@@ -1,13 +1,14 @@
-public class MultiDimensionalArray{
+public class MDLength{
 	public static void main(String[] args){
 		int[][] a = {{00,01,02},{10,11,12}};
-		for(int i =0; i<2;i++){
+		for(int i =0; i<a.length;i++){
 
-			for(int j=0;j<3;j++){
+			for(int j=0;j<a[0].length;j++){
 			System.out.print(a[i][j]+"\t");
 			}
 			System.out.println("");
 		}
 	
+	System.out.println("a[i][j] length:"+a.length+","+a[0].length);
 	}
 }
